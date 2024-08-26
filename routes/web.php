6e6195b7cb4f2a70379eb7c2b6f24b936285fcff
dedencreatifdeveloper <dedencreatifdeveloper@gmail.com>
2024-08-26 08:46:46 +0000

    Up

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layouts.app')
+<<<<<<< HEAD
     ->with('title','Dashboards');
+=======
+    ->with('title','DashboarD');
+>>>>>>> 7acd419 (Up)
 });
